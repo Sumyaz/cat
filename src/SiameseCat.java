e708d4163053a823a4504e7736ce3c6b1d54225a
Sumyaz <144610726+Sumyaz@users.noreply.github.com>
2024-03-28 08:16:17 -0400

    Update SiameseCat.java

--- src/SiameseCat.java
+++ src/SiameseCat.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 public class SiameseCat extends Cat {
-    // Additional attributes specific to SiameseCat
     private String eyeColor;
     public SiameseCat(String name, String color, String eyeColor) {
         super(name, "Siamese", color);
